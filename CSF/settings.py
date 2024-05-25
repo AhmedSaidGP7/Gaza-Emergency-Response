fa5d7d6c28588803789eae50f804b0731b5134d8
ahmedsaidgp7 <ahmed.saidgp7@gmail.com>
2024-05-26 02:17:39 +0300

    Test static

--- CSF/settings.py
+++ CSF/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
-"""
+""" 
 
 from pathlib import Path
 import os
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2^!-5c6vm)=vn7xe9u*6=kuq4%97l(dzwcj+6=b632cya1y^f8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["emregencyrresponsegaza.azurewebsites.net"]
+ALLOWED_HOSTS = ["emregencyrresponsegaza.azurewebsites.net", "127.0.0.1"]
 
 
 # Application definition
@@ -126,5 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
