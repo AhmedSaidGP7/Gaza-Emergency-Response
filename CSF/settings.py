1c999a3a4f398a09ee3899ecebf01153f5240f9a
ahmedsaidgp7 <ahmed.saidgp7@gmail.com>
2024-06-14 20:48:57 +0300

    trying to add csp 3

--- CSF/settings.py
+++ CSF/settings.py
@@ -48,12 +48,12 @@ CSRF_COOKIE_SECURE = True
 
 # Configure CSP settings
 
-CSP_DEFAULT_SRC = ("'self'",)
-CSP_IMG_SRC = ("'self'", "https://i.imgur.com", "https://fonts.gstatic.com")
-CSP_OBJECT_SRC = ("'none'",)
-CSP_SCRIPT_SRC = ("'self'", "https://cdn.jsdelivr.net", "https://code.jquery.com")
-CSP_STYLE_SRC = ("'self'", "https://fonts.googleapis.com")
-CSP_FONT_SRC = ("'self'", "https://fonts.gstatic.com")
+#CSP_DEFAULT_SRC = ("'self'",)
+#CSP_IMG_SRC = ("'self'", "https://i.imgur.com", "https://fonts.gstatic.com")
+#CSP_OBJECT_SRC = ("'none'",)
+#CSP_SCRIPT_SRC = ("'self'", "https://cdn.jsdelivr.net", "https://code.jquery.com")
+#CSP_STYLE_SRC = ("'self'", "https://fonts.googleapis.com")
+# CSP_FONT_SRC = ("'self'", "https://fonts.gstatic.com")
 # CSP_CONNECT_SRC = ("'self'", "https://fonts.googleapis.com")
 
 
