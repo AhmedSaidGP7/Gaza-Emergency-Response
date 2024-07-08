ef5dd9682dea2cfbcb35f806319c17a03643519e
ahmedsaidgp7 <ahmed.saidgp7@gmail.com>
2024-07-08 11:02:01 +0300

    trying to fix the deploy error, removing import cv2 from gaza models

--- GazaResponse/models.py
+++ GazaResponse/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-import pytesseract
-import re
+
 # Create your models here.
 
 # This class represents the governorates with which we deal, whether they have service centers or hospitals.
