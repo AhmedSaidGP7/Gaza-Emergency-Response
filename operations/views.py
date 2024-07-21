2c9f8f7bfe075cf839286a2841779b1ec0bbbcd5
ahmedsaidgp7 <ahmed.saidgp7@gmail.com>
2024-07-21 17:10:08 +0300

    import pd

--- operations/views.py
+++ operations/views.py
@@ -11,15 +11,15 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from datetime import datetime, timedelta
 from django.core.files.uploadedfile import SimpleUploadedFile
-# import pandas as pd
-# from django.core.exceptions import ObjectDoesNotExist  
-# from dateutil import parser
-# from django.core.files.storage import FileSystemStorage
-# import pytesseract
-# from PIL import Image
-# import re
-# from .forms import *
-# from django.db import transaction
+import pandas as pd
+from django.core.exceptions import ObjectDoesNotExist  
+from dateutil import parser
+from django.core.files.storage import FileSystemStorage
+import pytesseract
+from PIL import Image
+import re
+from .forms import *
+from django.db import transaction
 import os
 
 
