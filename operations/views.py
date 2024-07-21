e5f435c3a99ca65b5990bebaf3a76a49fc0dadf9
ahmedsaidgp7 <ahmed.saidgp7@gmail.com>
2024-07-22 02:31:26 +0300

    removing import forms

--- operations/views.py
+++ operations/views.py
@@ -10,13 +10,13 @@ from django.db.models import Sum, Count
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from datetime import datetime, timedelta
-# import pandas as pd
-# from dateutil import parser
+import pandas as pd
+from dateutil import parser
 from django.core.exceptions import ObjectDoesNotExist  
 from .utils import validate_date_format
 #from .forms import *
-#from django.db import transaction
-#import os
+from django.db import transaction
+import os
 
 
 @login_required
